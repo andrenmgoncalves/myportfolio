e05ce39b2df317e136e2e3caa398c8728b1d60d2
Andre Goncalves <andrenunomg@gmail.com>
2022-04-19 01:25:59 +0100

    Delete DontShutApp

--- dontshutapp/src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBPtMAQPu82C0WKl6Wrp14GEbbPi7MnB-k",
-    authDomain: "dontshutapp.firebaseapp.com",
-    projectId: "dontshutapp",
-    storageBucket: "dontshutapp.appspot.com",
-    messagingSenderId: "242973786978",
-    appId: "1:242973786978:web:8087089f437ad4bc33e23b"
-};
